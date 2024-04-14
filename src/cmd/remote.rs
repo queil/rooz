816842f9f5d376ab0568965eac6aebbaec123879
queil <queil@users.noreply.github.com>
2024-04-14 21:12:16 +0100

    add: auto port-forwarding

--- src/cmd/remote.rs
+++ src/cmd/remote.rs
@@ -1,12 +1,26 @@
-use futures::channel::oneshot::{self, Sender};
+use bollard::{
+    container::ListContainersOptions,
+    models::{Port, PortTypeEnum},
+    Docker,
+};
 use openssh::{ForwardType, KnownHosts, SessionBuilder};
 use regex::Regex;
-use std::{fs, path::Path, sync::Mutex, time::Duration};
+use std::{
+    collections::HashSet,
+    fs,
+    net::{Ipv4Addr, TcpListener},
+    path::Path,
+    sync::{
+        mpsc::{self, Sender},
+        Mutex,
+    },
+    time::Duration,
+};
 
-use crate::model::types::AnyError;
+use crate::{labels, model::types::AnyError};
 
 pub async fn remote(ssh_url: &str, local_docker_host: &str) -> Result<(), AnyError> {
-    let (sender, receiver) = oneshot::channel::<()>();
+    let (sender, receiver) = mpsc::channel::<()>();
 
     let tx_mutex = Mutex::<Option<Sender<()>>>::new(Some(sender));
 
@@ -31,7 +45,7 @@ pub async fn remote(ssh_url: &str, local_docker_host: &str) -> Result<(), AnyErr
     let session = SessionBuilder::default()
         .known_hosts_check(KnownHosts::Accept)
         .connect_timeout(Duration::from_secs(5))
-        .connect(&ssh_url)
+        .connect_mux(&ssh_url)
         .await?;
 
     println!("SSH: connected to {}", &ssh_url);
@@ -72,10 +86,78 @@ pub async fn remote(ssh_url: &str, local_docker_host: &str) -> Result<(), AnyErr
         local_socket_path.display()
     );
 
-    futures::executor::block_on(receiver)?;
+    let docker = Docker::connect_with_local_defaults()?;
+    let mut tunnels = HashSet::<u16>::new();
+    loop {
+        let containers = docker
+            .list_containers(Some(ListContainersOptions {
+                filters: (&labels::Labels::new(None, None)).into(),
+                ..Default::default()
+            }))
+            .await?;
 
-    if local_socket_path.exists() {
-        fs::remove_file(local_socket_path)?;
+        for (name, ports) in containers.iter().map(|c| {
+            let names = c
+                .names
+                .as_ref()
+                .map(|n| n.concat())
+                .unwrap_or(c.id.as_ref().unwrap().to_string());
+            let ports = c.clone().ports.unwrap_or(Vec::<_>::new());
+            (names.to_string(), ports)
+        }) {
+            for Port {
+                ip,
+                private_port,
+                public_port,
+                typ,
+            } in ports
+            {
+                let public_port = public_port.unwrap_or(private_port);
+                log::debug!(
+                    "{} {} {} {} {}",
+                    name,
+                    ip.unwrap_or_default(),
+                    private_port,
+                    public_port,
+                    typ.unwrap_or(PortTypeEnum::EMPTY)
+                );
+
+                let listen_socket = format!("127.0.0.1:{}", private_port);
+                let connect_socket = format!("127.0.0.1:{}", public_port);
+                session.check().await?;
+
+                if !tunnels.contains(&public_port) {
+                    if is_available(&private_port) {
+                        session
+                            .request_port_forward(
+                                ForwardType::Local,
+                                (Ipv4Addr::new(127, 0, 0, 1), private_port),
+                                (Ipv4Addr::new(127, 0, 0, 1), public_port),
+                            )
+                            .await?;
+                        println!(
+                            "Forwarding: {} -> {} ({})",
+                            listen_socket, connect_socket, name
+                        );
+                        tunnels.insert(public_port);
+                    } else {
+                        println!(
+                            "Already bound, so maybe forwarding: {} -> {} ({})",
+                            listen_socket, connect_socket, name
+                        );
+                        tunnels.insert(public_port);
+                    }
+                }
+            }
+        }
+        if let Some(()) = receiver.recv_timeout(Duration::from_secs(10)).ok() {
+            break;
+        }
     }
+    session.close().await?;
     std::process::exit(0);
+}
+
+fn is_available(port: &u16) -> bool {
+    TcpListener::bind(("127.0.0.1", *port)).ok().is_some()
 }
