895b4b118b9f69435711e4039933224daddc9b76
queil <queil@users.noreply.github.com>
2025-11-15 13:10:06 +0100

    fix: make system init --force acutally work sensibly I.e. stop trying to drop the ssh volume that will be in use by every single workspace container

--- src/cmd/init.rs
+++ src/cmd/init.rs
@@ -14,6 +14,29 @@ use crate::{
 use age::secrecy::ExposeSecret;
 
 impl<'a> InitApi<'a> {
+    async fn init_ssh(&self, image_id: &str, uid: &str) -> Result<(), AnyError> {
+        let hostname = self.client.info().await?.name.unwrap_or("unknown".into());
+        let init_ssh = format!(
+            r#"mkdir -p /tmp/.ssh
+                       KEYFILE=/tmp/.ssh/id_ed25519
+                       ls "$KEYFILE.pub" > /dev/null 2>&1 || ssh-keygen -t ed25519 -N '' -f $KEYFILE -C rooz@{}
+                       cat "$KEYFILE.pub"
+                       chmod 400 $KEYFILE && chown -R {} /tmp/.ssh
+                    "#,
+            &hostname, &uid,
+        );
+
+        self.container
+            .one_shot(
+                "init",
+                init_ssh,
+                Some(vec![ssh::mount("/tmp/.ssh")]),
+                None,
+                Some(&image_id),
+            )
+            .await
+    }
+
     pub async fn init(&self, image: &str, uid: &str, spec: &InitParams) -> Result<(), AnyError> {
         let image_id = self.image.ensure(&image, false).await?.id;
 
@@ -21,60 +44,41 @@ impl<'a> InitApi<'a> {
             None => age::x25519::Identity::generate(),
             Some(identity) => age::x25519::Identity::from_str(&identity)?,
         };
-        self.volume
-            .ensure_mounts(
-                &vec![RoozVolume::system_config_init(
-                    "/tmp/sys",
-                    SystemConfig {
-                        age_key: Some(age_key.to_string().expose_secret().to_string()),
-                        gitconfig: Some(
-                            r#"
+        if spec.force {
+            self.volume
+                .ensure_mounts(
+                    &vec![RoozVolume::system_config_init(
+                        "/tmp/sys",
+                        SystemConfig {
+                            age_key: Some(age_key.to_string().expose_secret().to_string()),
+                            gitconfig: Some(
+                                r#"
 [core]
   sshCommand = ssh -i /tmp/.ssh/id_ed25519 -o UserKnownHostsFile=/tmp/.ssh/known_hosts
 "#
-                            .trim()
-                            .to_string(),
-                        ),
-                    },
-                )?],
-                None,
-                Some(constants::ROOT_UID),
-            )
-            .await?;
-
+                                .trim()
+                                .to_string(),
+                            ),
+                        },
+                    )?],
+                    None,
+                    Some(constants::ROOT_UID),
+                )
+                .await?;
+        }
         match self
             .volume
             .ensure_volume(
                 ssh::VOLUME_NAME.into(),
-                spec.force,
+                false, // can't really recreate the volume if it is used by workspaces without dropping the workspaces
                 Some(Labels::from(&[Labels::role(
                     RoozVolumeRole::SshKey.as_str(),
                 )])),
             )
             .await?
         {
-            VolumeResult::Created { .. } => {
-                let hostname = self.client.info().await?.name.unwrap_or("unknown".into());
-                let init_ssh = format!(
-                    r#"mkdir -p /tmp/.ssh
-                       KEYFILE=/tmp/.ssh/id_ed25519
-                       ls "$KEYFILE.pub" > /dev/null 2>&1 || ssh-keygen -t ed25519 -N '' -f $KEYFILE -C rooz@{}
-                       cat "$KEYFILE.pub"
-                       chmod 400 $KEYFILE && chown -R {} /tmp/.ssh
-                    "#,
-                    &hostname, &uid,
-                );
-
-                self.container
-                    .one_shot(
-                        "init",
-                        init_ssh,
-                        Some(vec![ssh::mount("/tmp/.ssh")]),
-                        None,
-                        Some(&image_id),
-                    )
-                    .await?;
-            }
+            VolumeResult::Created { .. } => self.init_ssh(&image_id, uid).await?,
+            VolumeResult::AlreadyExists if spec.force => self.init_ssh(&image_id, uid).await?,
             VolumeResult::AlreadyExists => {
                 println!("Rooz has been already initialized. Use --force to reinitialize.")
             }
